c28f3a7d86853ebe5340aa6b6605715db5203642
zhaoxfan98 <zhaoxfan98@gmail.com>
2021-12-22 10:13:43 +0800

    stack

--- code/20.有效的括号.java
+++ code/20.有效的括号.java
@@ -6,8 +6,16 @@
 
 // @lc code=start
 class Solution {
+    
     public boolean isValid(String s) {
-
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '[') stack.push(']');
+            else if (ch == '(') stack.push(')');
+            else if (ch == '{') stack.push('}');
+            else if (stack.isEmpty() || ch != stack.pop()) return false;
+        }
+        return stack.isEmpty();
     }
 }
 // @lc code=end
--- code/232.用栈实现队列.java
+++ code/232.用栈实现队列.java
@@ -6,25 +6,40 @@
 
 // @lc code=start
 class MyQueue {
-
+    /**
+    两个栈，一个用来存放数据（数据栈），一个用来辅助（辅助栈）
+    */
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
     public MyQueue() {
-        
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();        
     }
     
     public void push(int x) {
-
+        stack1.push(x);
     }
     
     public int pop() {
-
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
     }
     
     public int peek() {
-
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
     }
     
     public boolean empty() {
-
+        return stack1.isEmpty() && stack2.isEmpty();
     }
 }
 
