5a22809d1be91c3f782b6750deb461379192bddf
zhaoxfan98 <zhaoxfan98@gmail.com>
2021-12-24 10:00:49 +0800

    binaryTree

--- code/101.对称二叉树.java
+++ code/101.对称二叉树.java
@@ -22,7 +22,14 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return cmp(root.left, root.right);
+    }
 
+    boolean cmp(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) return true;
+        if (node1 == null || node2 == null || node1.val != node2.val) return false;
+        return cmp(node1.left, node2.right) && cmp(node1.right, node2.left);
     }
 }
 // @lc code=end
--- code/102.二叉树的层序遍历.java
+++ code/102.二叉树的层序遍历.java
@@ -21,8 +21,26 @@
  * }
  */
 class Solution {
+
     public List<List<Integer>> levelOrder(TreeNode root) {
-        
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (root == null) return res;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int currentLevelSize = queue.size();
+            for (int i=1; i<=currentLevelSize; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(level);
+        }
+        return res;
+
     }
 }
 // @lc code=end
